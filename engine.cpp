c736d0679fe3ac462ec42ab5ac93deb7a708933f
julius-42 <kundratjulius@gmail.com>
2026-05-06 00:32:57 +0200

    added cube projection with lines

--- engine.cpp
+++ engine.cpp
@@ -7,9 +7,18 @@ Compiled: g++ -std=c++20 engine.cpp -o engine -lSDL2
 
 #include <SDL2/SDL.h>
 
+#define HEIGHT 720
+#define WIDTH 720
+
+
 class Color{
     public:
         unsigned R, G, B;
+};
+
+class Point{
+    public:
+        int X,Y;
 };
 
 void draw_sphere(SDL_Renderer* rndr, int x, int y, int r, Color clr){
@@ -23,6 +32,46 @@ void draw_sphere(SDL_Renderer* rndr, int x, int y, int r, Color clr){
     }
 }
 
+// initial cords are 0..x 0..y -> projection uses -x/2..x/2  -y/2..y/2
+// -x/2..x/2 +x/2 0..x
+Point project_point(SDL_Renderer* rndr, float x, float y, float z, Color clr){
+    float fov = 100;
+    float pX = (x/z * fov)+WIDTH/2;
+    float pY = (y/z * fov)+HEIGHT/2;
+
+    draw_sphere(rndr, (int)(pX), (int)(pY), 4, clr);
+
+    Point p = {(int)pX,(int)pY};
+    return p;
+}
+
+
+void project_cube(SDL_Renderer* rndr, float x, float y, float z, float size, Color clr){
+    float r = size/2;
+
+    // 8 corners
+    float pts[8][3] = {
+        {x-r, y-r, z-r}, {x+r, y-r, z-r},
+        {x+r, y+r, z-r}, {x-r, y+r, z-r},  // front face
+        {x-r, y-r, z+r}, {x+r, y-r, z+r},
+        {x+r, y+r, z+r}, {x-r, y+r, z+r},  // back face
+    };
+
+    // 12 edges: front, back, connecting sides
+    int edges[12][2] = {
+        {0,1},{1,2},{2,3},{3,0},  // front face
+        {4,5},{5,6},{6,7},{7,4},  // back face
+        {0,4},{1,5},{2,6},{3,7},  // connecting edges
+    };
+
+    for(auto& edge : edges){
+        auto* a = pts[edge[0]];
+        auto* b = pts[edge[1]];
+        Point p1 = project_point(rndr, a[0],a[1],a[2], clr);
+        Point p2 = project_point(rndr, b[0],b[1],b[2], clr);
+        SDL_RenderDrawLine(rndr, p1.X, p1.Y, p2.X, p2.Y);
+    }
+}
 
 int main(){
 
@@ -35,19 +84,25 @@ int main(){
     SDL_Renderer* renderer = nullptr;
 
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_CreateWindowAndRenderer(640, 480, 0, &win, &renderer);
+    SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, 0, &win, &renderer);
 
     while(running){
 
+        // closes the window when 'X' pressed
         while(SDL_PollEvent(&e)){
             if(e.type == SDL_QUIT)
                 running = false;
         }
 
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        // fills window with color
+        SDL_SetRenderDrawColor(renderer, black.R, black.G, black.B, 255);
         SDL_RenderClear(renderer);
 
-        draw_sphere(renderer, 320, 240, 5, green);
+        SDL_SetRenderDrawColor(renderer, green.R, green.G, green.B, 255);
+        SDL_RenderDrawPoint(renderer, WIDTH/2, HEIGHT/2);
+
+        project_cube(renderer, 0, 0, 8, 10, green);
+        
 
         SDL_RenderPresent(renderer);
 
